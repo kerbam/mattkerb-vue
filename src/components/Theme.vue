54ca826d617472862cbf57deffe1b5031fa06e02
Matt Kerbawy <matt@pendo.io>
2024-06-14 13:39:06 -0400

    Add toggle button for easier theme switching

---
+++ src/components/Theme.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+
+    <p>isDark: {{ isDark }}</p>
+    <button @click="toggleDark()">Toggle theme</button>
+</template>
+
+<style scoped>
+button {
+    background: var(--ds-border-brand);
+    color: var(--ds-text);
+}
+</style>
